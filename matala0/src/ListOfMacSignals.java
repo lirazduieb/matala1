6fd6fe427075f2eb6af3b5dfdec4500011de00e4
annty2 <33619352+annty2@users.noreply.github.com>
2017-12-22 00:06:32 +0200

    Add files via upload

---
+++ matala0/src/ListOfMacSignals.java
@@ -0,0 +1,52 @@
+package matala2;
+//create a ListOfMacSignals that contains mac adress and signal
+
+
+
+
+
+
+
+public class ListOfMacSignals
+{
+		public String Mac;
+		public int Signal;
+		
+		public ListOfMacSignals(String Mac,int Signal)
+		{
+			this.Mac=Mac;
+			this.Signal=Signal;
+		}
+
+		//if algorithem 1 is not working
+		
+		public ListOfMacSignals(String Mac)
+
+		{
+			this.Mac=Mac;
+			this.Signal=-360000;
+		}
+		public String getMac() 
+		{
+			return Mac;
+		}
+		public void setMac(String mac) 
+		{
+			Mac = mac;
+		}
+		public int getSignal() 
+		{
+			return Signal;
+		}
+		public void setSignal(int signal)
+		{
+			Signal = signal;
+		}
+		public String toString() 
+		{
+			return "ListOfMacSignals [Mac=" + Mac + ", Signal=" + Signal + "]";
+		}
+
+	}
+
+
